047e5b3f5368f0a5a536d6e35bb9c3deb22d3f46
Diego Reis <reisdiego144@gmail.com>
2024-09-15 01:00:05 -0300

    feat: exercise 1

--- queries/ex1.js
+++ queries/ex1.js
@@ -1,1 +1,10 @@
-db.orders.findOne();
+db.products.find({
+    "standard_cost": {
+        $gt: 20,
+    }
+},
+    {
+        "_id": true,
+        "standard_cost": true,
+    }
+).sort({ "_id": 1 })
