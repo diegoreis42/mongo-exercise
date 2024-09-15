776971783d71b3a9d7d5a22d6a886d228e3a8c20
Diego Reis <reisdiego144@gmail.com>
2024-09-15 02:36:53 -0300

    refactor: remove 'northwind>' from output files

--- scripts/init-mongo.sh
+++ scripts/init-mongo.sh
@@ -13,7 +13,7 @@ mkdir -p /output
 for query_file in /queries/*.js; do
   query_name=$(basename "$query_file" .js)
 
-  cat $query_file | mongosh northwind --eval --quiet > "/output/${query_name}-result.json"
+  cat $query_file | mongosh northwind --eval --quiet  | sed 's/^northwind> //' > "/output/${query_name}-result.json"
 
   echo "Executed $query_file and saved the result to /output/${query_name}.json"
 done
